5b00124765f230f8bdf5fefd03ab70c2ecdc7ee0
FireCube <etiennedx00@gmail.com>
2019-02-12 10:54:57 +0100

    created files

--- src/index.php
+++ src/index.php
@@ -1,5 +1,12 @@
 <?php
 
+include '../includes/database.php';
+include '../includes/user.php';
+
+$sql = init_sql();
+
+$user = get_user($sql);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,9 +14,21 @@
     <meta charset="utf-8"/>
     <title>SOS'ial</title>
 
-    <link rel="stylesheet" href="../css/min.css"/>
+    <link rel="stylesheet" href="../css/main.css"/>
   </head>
   <body>
     <h1>SOS'ial</h1>
+    <?php
+      if($user["connected"]):// alternative syntax, easier inside html
+    ?>
+    <!--A user is connected-->
+    <?php
+      else:
+    ?>
+    <!--No connected user-->
+    <?php
+      endif;
+    ?>
+    <script src="../js/index.js"></script>
   </body>
 </html>
