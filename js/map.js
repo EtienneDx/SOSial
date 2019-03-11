c1299463ec7cd7f5924c83916e1f7a9573f82028
FireCube <etiennedx00@gmail.com>
2019-03-11 12:15:54 +0100

    Fix map

--- js/map.js
+++ js/map.js
@@ -1,53 +1,30 @@
-<!DOCTYPE html>
-<html>
-	<head>
-        <meta charset="utf-8">
-        <!-- Nous chargeons les fichiers CDN de Leaflet. Le CSS AVANT le JS -->
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
-            crossorigin="" />
-        <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
-            crossorigin=""></script>
-		<script type="text/javascript">
-			// On initialise la latitude et la longitude de Paris (centre de la carte)
-			var lat = 48.852969;
-			var lon = 2.349903;
-			var macarte = null;
-      var villes = {
-	    "Paris": { "lat": 48.852969, "lon": 2.349903 },
-	    "Brest": { "lat": 48.383, "lon": -4.500 },
-	    "Quimper": { "lat": 48.000, "lon": -4.100 },
-	    "Bayonne": { "lat": 43.500, "lon": -1.467 }
-      };
-			// Fonction d'initialisation de la carte
-			function initMap() {
-				// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-                macarte = L.map('map').setView([lat, lon], 11);
-                // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-                    // Il est toujours bien de laisser le lien vers la source des données
-                    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-                    minZoom: 1,
-                    maxZoom: 20
-                }).addTo(macarte);
-                for (ville in villes) {
-		                var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
-                }    
-            }
-			window.onload = function(){
-				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-				initMap(); 
-			};
-		</script>
-		<style type="text/css">
-			#map{ /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
-				height:400px;
-			}
-		</style>
-		<title>Carte</title>
-	</head>
-	<body>
-		<div id="map">
-			<!-- Ici s'affichera la carte -->
-		</div>
-	</body>
-</html>
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 48.852969;
+var lon = 2.349903;
+var macarte = null;
+var villes = {
+	"Paris": { "lat": 48.852969, "lon": 2.349903 },
+	"Brest": { "lat": 48.383, "lon": -4.500 },
+	"Quimper": { "lat": 48.000, "lon": -4.100 },
+	"Bayonne": { "lat": 43.500, "lon": -1.467 }
+};
+// Fonction d'initialisation de la carte
+function initMap()
+{
+	// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+	macarte = L.map('map').setView([lat, lon], 11);
+	// Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+	L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+		// Il est toujours bien de laisser le lien vers la source des données
+		attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+		minZoom: 1,
+		maxZoom: 20
+	}).addTo(macarte);
+	for (ville in villes) {
+		var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
+	}
+}
+window.onload = function(){
+	// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+	initMap();
+};
--- src/map.php
+++ src/map.php
@@ -1,5 +1,40 @@
 <?php
 
-<iframe src="https://www.google.com/maps/embed?pb=" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <!-- Nous chargeons les fichiers CDN de Leaflet. Le CSS AVANT le JS -->
+    <link
+      rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+      integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+      crossorigin=""
+    />
+
+		<style type="text/css">
+			#map
+      { /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
+				height:400px;
+			}
+		</style>
+		<title>Carte</title>
+	</head>
+	<body>
+		<div id="map">
+			<!--<iframe src="https://www.google.com/maps/embed?pb=" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+      </iframe>-->
+		</div>
+
+
+    <script
+      src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+      integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+      crossorigin=""
+    >
+    </script>
+    <script type="text/javascript" src="../js/map.js"></script>
+	</body>
+</html>
