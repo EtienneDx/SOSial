c8b0e35f0d43f2bcb02d51f30cf2a4a52f9fc947
EtienneDx <etiennedx00@gmail.com>
2019-05-11 13:05:13 +0200

    Added map to create event

---
+++ js/createEvent.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('#datePicker').datetimepicker();// activate the datetime datePicker
+
+  $('#createEvent').submit((e) => {
+    if(typeof marker == "undefined")
+    {
+      e.preventDefault();
+      displayError("Vous devez indiquer la position de l'évènement!");
+      return false;
+    }
+  });
+
+  /*********MAP INIT************/
+
+  map = L.map('map').setView([48.852969, 2.349903], 11);// default is paris
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+		// Il est toujours bien de laisser le lien vers la source des données
+		attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+		minZoom: 1,
+		maxZoom: 20
+	}).addTo(map);
+
+  map.on('click', (e) => {
+    $('#latitude').val(e.latlng.lat);
+    $('#longitude').val(e.latlng.lng);
+
+    if(marker == null)
+    {
+      marker = L.marker(e.latlng).addTo(map);
+    }
+    else
+    {
+      marker.setLatLng(e.latlng);
+    }
+  })
+});
+
+function displayError(msg)
+{
+  $('.alert').html(msg).show();
+}
--- src/createEvent.php
+++ src/createEvent.php
@@ -18,7 +18,7 @@ if(!$user["connected"] || $user['role'] !== 1)// redirect unconnected
 if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['date']))
 {
   $prep = $mysqli->prepare("INSERT INTO `events` (`author_id`, `name`, `description`, `latitude`, `longitude`, `date`) VALUES (?, ?, ?, ?, ?, ?)");
-  $prep->bind_param("issdds", intval($user['id']), htmlspecialchars($_POST['name']), htmlspecialchars($_POST['description']), intval($_POST['latitude']), intval($_POST['longitude']), $_POST['date']);
+  $prep->bind_param("issdds", intval($user['id']), htmlspecialchars($_POST['name']), htmlspecialchars($_POST['description']), doubleval($_POST['latitude']), doubleval($_POST['longitude']), $_POST['date']);
 
   $prep->execute();
 
@@ -43,7 +43,23 @@ if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['latitu
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="../css/bootstrap-datetimepicker.min.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+       integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+       crossorigin=""/>
     <link rel="stylesheet" href="../css/main.css"/>
+    <style>
+    #latitude, #longitude /*Hidden fields, will get filled by js*/
+    {
+      display: none;
+    }
+
+    #map
+    {
+      width: 100%;
+      height: 200px;
+      overflow: hidden;
+    }
+    </style>
   </head>
   <body>
     <div class="container">
@@ -53,8 +69,11 @@ if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['latitu
         </div>
       </div>
       <div class="row">
+        <div class="col-md-6 offset-md-3 col-sm-12 alert alert-danger" style="display: none;"></div>
+      </div>
+      <div class="row">
         <div class="col-md-6 offset-md-3 col-sm-12">
-          <form target="_self" method="POST">
+          <form target="_self" method="POST" id="createEvent">
             <div class="form-group">
               <label for="name">Nom : </label>
               <input type="text" name="name" id="name" class="form-control"></input>
@@ -63,13 +82,10 @@ if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['latitu
               <label for="description">Description : </label>
               <textarea name="description" id="description" class="form-control"></textarea>
             </div>
-            <div class="form-group">
-              <label for="longitude">Longitude : </label>
-              <input type="number" name="longitude" id="longitude" class="form-control"></input>
-            </div>
-            <div class="form-group">
-              <label for="latitude">Latitude : </label>
-              <input type="number" name="latitude" id="latitude" class="form-control"></input>
+            <input type="hidden" name="longitude" id="longitude" class="form-control"></input>
+            <input type="hidden" name="latitude" id="latitude" class="form-control"></input>
+            <div id="map">
+
             </div>
             <div class="form-group">
               <label for="datePicker">Date : </label>
@@ -84,10 +100,12 @@ if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['latitu
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="../js/bootstrap-datetimepicker.min.js"></script>
-    <script>
-    $(function () {
-      $('#datePicker').datetimepicker();
-    });
+    <script
+      src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+      integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+      crossorigin=""
+    >
     </script>
+    <script src="../js/createEvent.js"></script>
   </body>
 </html>
